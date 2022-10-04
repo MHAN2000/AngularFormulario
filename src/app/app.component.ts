c5f90c34591cf6c710f4d0c15f7ebe979a93ab32
mhan2000 <betitoas3@hotmail.com>
2022-10-04 17:23:31 -0500

    Martes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  registerForm !: FormGroup;
   title = 'AngularFormulario';
+  submitted = false;
+  constructor(private formBuilder: FormBuilder) { }
+  ngOnInit() {
+    this.registerForm = this.formBuilder.group({
+      firstName: ['', Validators.required],
+      lastName: ['', Validators.required],
+      email: ['', Validators.required],
+      password: ['', Validators.required, Validators.minLength(6)],
+    });
+  }
+
+  onSubmit() {
+    this.submitted = true;
+    if (this.registerForm.invalid)
+      return;
+      
+    alert('Success');
+  }
 }
